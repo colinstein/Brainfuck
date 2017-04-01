b2489898f3780ae2417dcf2288f49f688500dfa2
Colin Stein <colinstein@me.com>
2017-04-01 16:53:02 -0400

    Minor refactor and commenting for Program

--- lib/brainfuck/version.rb
+++ lib/brainfuck/version.rb
@@ -2,5 +2,5 @@ module Brainfuck
   # This version is updated manually and may not map 1:1 with with commits to
   # master. The version will not change on minor issues like documentation
   # updates or whitespace changes in the code.
-  VERSION = "0.2.2"
+  VERSION = "0.2.3"
 end
